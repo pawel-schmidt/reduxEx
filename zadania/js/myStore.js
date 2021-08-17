26b1627916e2eb18578119e955540cee391dad15
michaljedro <michal.jedro@wp.pl>
2021-08-18 00:23:58 +0200

    ZADANKA

---
+++ zadania/js/myStore.js
@@ -0,0 +1,15 @@
+/*
+Zaimplementuj własny Store na bazie API, które udostępnia reduxowy Store
+
+Store {
+    getState,
+    dispatch,
+    subscribe
+}
+ */
+
+class Store {
+
+}
+
+export default Store;
